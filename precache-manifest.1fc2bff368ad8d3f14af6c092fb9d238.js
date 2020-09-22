c1eae8f595d42dba78a58c18c5aca3bca997cbe1
tisoga <tisoga@gmail.com>
2020-09-22 10:20:30 +0700

    Updates

---
+++ precache-manifest.1fc2bff368ad8d3f14af6c092fb9d238.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2060c582404fe2c149c8a3c8628c3aa3",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "9a78be9a3c4f70b16c32",
+    "url": "/portofolio/static/css/main.c1c0f5da.chunk.css"
+  },
+  {
+    "revision": "4ebc183d2e503dfc1bd3",
+    "url": "/portofolio/static/js/2.8505ee34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portofolio/static/js/2.8505ee34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a78be9a3c4f70b16c32",
+    "url": "/portofolio/static/js/main.48f80d45.chunk.js"
+  },
+  {
+    "revision": "a3a6c8a1f81f721d2b09",
+    "url": "/portofolio/static/js/runtime-main.cef59536.js"
+  },
+  {
+    "revision": "67fb51a38765d69ae1f50520c735f7fc",
+    "url": "/portofolio/static/media/github-logo.67fb51a3.svg"
+  },
+  {
+    "revision": "3c71cddad95f5be74eb50bc94f5ad0cf",
+    "url": "/portofolio/static/media/python.3c71cdda.png"
+  },
+  {
+    "revision": "a0e06ed8176b82b57aab59f8de2d459c",
+    "url": "/portofolio/static/media/rest.a0e06ed8.svg"
+  },
+  {
+    "revision": "7de5888e4fe855bc963c6d4132500601",
+    "url": "/portofolio/static/media/web.7de5888e.png"
+  }
+]);
